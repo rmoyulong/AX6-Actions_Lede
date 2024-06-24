2e7b29168265d712fb6d00a8905a625529025cac
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-24 19:41:13 +0800

    增加固件files文件夹集合 2024-06-24

--- union_files/onecloud/files/etc/1.sh
+++ union_files/onecloud/files/etc/1.sh
@@ -8,10 +8,10 @@ echo "# Put your custom commands here that should be executed once
 
 #mount -o remount rw /
 #/usr/sbin/balethirq.pl
-#alias reboot='reboot -f'
-sh /etc/diymo.sh
-sh /etc/aria2.sh
+bash /etc/diymo.sh
+bash /etc/aria2.sh
+
 exit 0">/etc/rc.local
 
 rm -rf /etc/1.sh
-sh /etc/rc.local
+bash /etc/rc.local
